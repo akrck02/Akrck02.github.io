2487ed8f21894b82e7037e14c6f403dccc1e9518
akrck02 <aketza.vazquez@gmail.com>
2021-09-20 10:48:52 +0200

    Starting migration

---
+++ old/README.md
@@ -0,0 +1,2 @@
+# Akrck02.com
+Website to show my projects
---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import { CONFIG, PATHS, setUpConfigurations } from './config/config';
+import { load } from './views/router';
+
+const loadFromUrl = () => {
+    // get the url paramaters or routes and load the page
+    const params = [] // getParametersByIndex(window.location.hash.slice(1).toLowerCase(),1);
+    //if(token) check token 
+
+    if(params[0] == undefined){
+        location.href = CONFIG.URL + "#/";
+        load([""]);
+    }
+    else {
+        console.log(CONFIG);
+        console.log(PATHS);
+         load(params);
+    }
+}
+
+window.addEventListener('hashchange',() =>{
+    
+    loadFromUrl();
+});
+
+window.onload = () => {  
+    setUpConfigurations();
+    loadFromUrl();
+}
