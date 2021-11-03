a2b297796fc4c2b976d05784d0f8c31287222034
akrck02 <aketza.vazquez@gmail.com>
2021-11-03 18:06:47 +0100

    [Core] Event detecting system added, LET THEM KNOW ITS CHRISTMAS TIME

--- src/views/error/errorV.ts
+++ src/views/error/errorV.ts
@@ -1,4 +1,4 @@
-import { CONFIG } from "../../config/config.js";
+import { CONFIG, VIEWS } from "../../config/config.js";
 import { ERRORS } from "../../config/errors.js";
 import { getParametersByBreakPoint } from "../../lib/gtd/data/urltools.js";
 import { UIComponent } from "../../lib/gtd/web/uicomponent.js";
@@ -19,16 +19,20 @@ export default function show(params : string[]) : void {
             height : "100%", 
             justifyContent: "center",
             alignItems: "center",
+            transition: ".25s",
+            opacity: "0",
         }
     });
 
     const message = new UIComponent({
-        type : "h1",
+        type : "hp",
         text : "ERROR " + error.code + ": " + error.description,
         styles : {
             width: "80%",
             maxWidth: "500px",
-            textAlign: "center"
+            textAlign: "center",
+            fontSize: "1.5em",
+            margin: "15px 0",
         }
     });
 
@@ -47,9 +51,36 @@ export default function show(params : string[]) : void {
         }
     });
 
+    const back = new UIComponent({
+        type : "button",
+        text : "Back to home",
+        styles : {
+            width: "100%",
+            maxWidth: "150px",
+            marginTop: "5px",
+            marginBottom: "5px",
+            padding: "10px",
+            borderRadius: "5px",
+            border: "none",
+            boxShadow: "0px 2px 4px rgba(0,0,0,.15)",
+            textAlign: "center",
+            fontWeight: "bold", 
+            cursor: "pointer",  
+        },
+        events : {
+            click : () =>{
+                location.href = VIEWS.BASE_URL;
+            }
+        }
+    });
+
+    setTimeout(() => {
+        view.element.style.opacity = "1";
+    }, 200);
+
     picture.appendTo(view);
     message.appendTo(view);
-
+    back.appendTo(view);
 
     view.appendTo(document.body);
 
