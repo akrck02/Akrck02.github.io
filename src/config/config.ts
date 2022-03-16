b17c063725620b0fe7a4a76c7b05597bf13ab9f9
akrck02 <aketza.vazquez@gmail.com>
2022-03-16 16:37:21 +0100

    [config] Akrck02.com expired switch to akrck02.github.io

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@
 export const CONFIG  = {
     APP_NAME: 'Akrck02.com',
     APP_VERSION: 'v1.0',
-    HOST: 'akrck02.github.com',
+    HOST: 'akrck02.github.io',
     PORT: 80,
     URL: '',
     ENVIROMENT : 'development',
